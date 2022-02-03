672be95f51d70259d30e6cccbafaad2c66103d09
Dmitriy Polyakov <dmitriyap11@gmail.com>
2022-02-04 00:30:49 +0300

    Try flow coordinator

--- Demo/DPUIKitDemo/AppDelegate.swift
+++ Demo/DPUIKitDemo/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         self.window = .init()
-        self.window?.rootViewController = UINavigationController(rootViewController: DemoMainViewController()) //DemoPageContainerViewController()
+        self.window?.rootViewController = AppFlowViewController()//UINavigationController(rootViewController: DemoMainViewController())
         self.window?.makeKeyAndVisible()
         
         return true
